4f153bebf9c5315b1eca0328e5728291d1478383
Fahri Cihan Demirci <femnad@users.noreply.github.com>
2020-01-10 00:48:54 +0300

    add variables file

--- main.tf
+++ main.tf
@@ -1,35 +1,3 @@
-variable prefix {}
-
-variable github_user {}
-
-variable project {}
-
-variable service_account_file {}
-
-variable ssh_user {}
-
-variable ssh_email {}
-
-variable image {
-  default = "debian-cloud/debian-10"
-}
-
-variable machine_type {
-  default = "g1-small"
-}
-
-variable region {
-  default = "europe-west-2"
-}
-
-variable zone {
-  default = "europe-west2-c"
-}
-
-variable managed_zone {}
-
-variable dns_name {}
-
 data "http" "ipinfo" {
   url = "https://ipinfo.io/json"
 }
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable prefix {}
+
+variable github_user {}
+
+variable project {}
+
+variable service_account_file {}
+
+variable ssh_user {}
+
+variable ssh_email {}
+
+variable managed_zone {}
+
+variable dns_name {}
+
+variable image {
+  default = "debian-cloud/debian-10"
+}
+
+variable machine_type {
+  default = "g1-small"
+}
+
+variable region {
+  default = "europe-west-2"
+}
+
+variable zone {
+  default = "europe-west2-c"
+}
